548ce49c356f41cee991928df2de8f3ba0af862d
Jason Harmon <jasharmon@paypal.com>
2014-08-29 15:27:23 -0500

    Added missing new exception

---
+++ src/main/java/com/paperbackswap/exceptions/BookListBuilderException.java
@@ -0,0 +1,9 @@
+package com.paperbackswap.exceptions;
+
+public class BookListBuilderException extends Exception {
+	private static final long serialVersionUID = 1979184620986079309L;
+	
+	public BookListBuilderException(String message) {
+		super(message);
+	}
+}
